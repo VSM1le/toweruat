e5db8921428d4fb7d7bf10f98e2cb95044683944
VSM1le <earthfg@gmail.com>
2026-05-27 17:04:02 +0700

    [fix] change app logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,8 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
                 <a href="/" wire:navigate>
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <!-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> -->
+                      <img src="btu-remove-bg.png" width="150" height="75" alt="BTU icon">
                 </a>
             </div>
 
